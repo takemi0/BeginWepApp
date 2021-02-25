658fe8e20593b28ba37cdc296ec7a336790f87e5
健巳(Takemi) <54488068+takemi0@users.noreply.github.com>
2021-02-25 18:17:34 +0900

    #8 の検証前コミット

---
+++ src/008/index.php
@@ -0,0 +1,139 @@
+<?php
+
+//月の日データ
+$data = [ 0 =>  ['', '','','','','',''] ];
+
+$trg_date = null;
+//URL引数のtarget_dateを取得する
+if( isset( $_GET['target_date'] ) && !empty( $_GET['target_date'] ) ) {
+	$trg_date = strtotime( $_GET['target_date'] );
+	if( $trg_date === false || count( explode('-', $_GET['target_date']) ) < 3) {
+		$trg_date = time();
+	}
+} else {
+	$trg_date = time();
+}
+
+//データ作成
+$today = [
+	'y' => date('Y'),
+	'm' => date('m'),
+	'd' => date('d'),
+];
+$trg = [
+	'y' => date('Y', $trg_date ),
+	'm' => date('m', $trg_date ),
+];
+$begin = strtotime( date('Y-m-1', $trg_date) );
+$end = strtotime( date('Y-m-t', $trg_date) );
+$line = 0;
+for( $d = $begin; $d <= $end; $d += 24 * 3600 ) {
+	$w = date( 'w', $d );
+	$i = $w - 1;
+	if( $i < 0 ) $i = 6;
+	//echo date('Y-m-d', $d); echo " {$i} \n";
+	$data[$line][$i] = date( 'd', $d );
+	if( $i == 6 ) { 
+		$line ++;
+		$data[$line] = [ '', '', '', '', '', '', '' ];
+	}
+}
+
+//祝日処理
+$holidays = [];
+$fp = fopen( 'holiday.csv', 'r' );
+while( $hdata = fgetcsv($fp) !== false ) {
+	if( !$hdata ) continue;
+	$c_time = strtotime( $hdata[0] );
+	$c_year = date('Y', $c_time );
+	$c_month = date( 'm', $c_time );
+	$c_day = date( 'd', $c_time );
+	//処理月と異なる年付きの祝日データは読み飛ばし
+	if( !($trg['h'] == $c_year && $trg['m'] == $c_month) ) continue;
+
+	$holidays[$c_day] = $hdata[1];
+}
+
+$rows = [];
+foreach( $data as $l ) {
+	$tmp = "<tr>";
+	foreach( $l as $i => $d ) {
+		$class = "cl_day";
+		if( !$d ) {
+			$class = "cl_none";
+		} else {
+			if( $i == 5 ) {
+				$class = "cl_sat";
+			} elseif( $i == 6 ) {
+				$class = "cl_sun";
+			}
+		}
+
+		if( $trg['y'] == $today['y'] && 
+			$trg['m'] == $today['m'] &&
+				   $d == $today['d']
+		) { 
+			$class .= " cl_today";
+		}
+
+		if( !empty($holidays[$d]) ) {
+			$class .= " cl_holiday";
+		}
+
+		$tmp.= "<td class='{$class}'>";
+		$tmp.= $d;
+		if( !empty($holidays[$d]) ) {
+			$tmp.= '<br/>'.$holidays[$d];
+		}
+		$tmp.= "</td>";
+	}
+	$tmp.= "</tr>";
+	$rows[] = $tmp;
+}
+
+$tmp =  file_get_contents('index.tpl');
+
+$tmp = str_replace( '{$today}', date('Y-m-d'), $tmp );
+$tmp = str_replace( '{$next_month}', getNextMonth( $trg_date), $tmp );
+$tmp = str_replace( '{$prev_month}', getPrevMonth( $trg_date), $tmp );
+
+$tmp = str_replace( '{$year}', date('Y',$trg_date), $tmp );
+$tmp = str_replace( '{$month}', date('m',$trg_date), $tmp );
+$tmp = str_replace( '{$week_date}', implode('',$rows), $tmp );
+
+echo $tmp;
+exit;
+
+/**
+ * 前月の1日の日付文字列を取得
+ *
+ * @param [type] $prc_date
+ * @return void
+ */
+function getPrevMonth( $proc_date ) {
+	$year = date( 'Y', $proc_date );
+	$month= date( 'm', $proc_date );
+	$month --;
+	if( $month < 1 ) {
+		$year --;
+		$month = 12;
+	}
+	return "{$year}-{$month}-1";
+}
+
+/**
+ * 翌月の1日の日付文字列を取得
+ *
+ * @param [type] $proc_date
+ * @return void
+ */
+function getNextMonth( $proc_date ) {
+	$year = date( 'Y', $proc_date );
+	$month= date( 'm', $proc_date );
+	$month ++;
+	if( $month > 12 ) {
+		$year ++;
+		$month = 1;
+	}
+	return "{$year}-{$month}-1";
+}
---
+++ src/008/index.tpl
@@ -0,0 +1,64 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html lang="ja">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<title>サンプルアプリ：カレンダー</title>
+	<link rel="stylesheet" href="index.css">
+	<link rel="stylesheet" href="calender.css">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="/bootstrap_4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+	 <!-- Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+		<div class="flex-center position-ref full-height">
+			<div class="top-right links">
+				カレンダーアプリ<br/>
+				Version: 1.0.1
+			</div>
+
+			<div class="content">
+				<div class="title m-b-md">
+					カレンダーアプリ
+				</div>
+				<div class="m-b-md">
+					<div class="calender">
+					<table class="calender">
+						<tr>
+							<th colspan="2" style="text-align: center;"><a href="?target_date={$prev_month}">前月</a></th>
+							<th colspan="3" style="text-align: center;">
+								{$year}年 {$month}月<br/>
+								<a href="?target_date={$today}">今日</a>
+							</th>
+							<th colspan="2" style="text-align: center;"><a href="?target_date={$next_month}">翌月</a></th>
+						</tr>
+						<tr>
+							<th class="cl_head">月</th>
+							<th class="cl_head">火</th>
+							<th class="cl_head">水</th>
+							<th class="cl_head">木</th>
+							<th class="cl_head">金</th>
+							<th class="cl_head">土</th>
+							<th class="cl_head">日</th>
+						</tr>
+						{$week_date}
+					</table>
+					</div>
+				</div>
+			</div>
+			<div class="footer">
+				&copy; 健巳 (Takemi)
+				<a target="_blank" href="https://twitter.com/takemi77505234"><img src="../img/twitter_icon-icons.com_62765.png" width="16"></a>&nbsp;
+				<a target="_blank" href="https://www.youtube.com/channel/UC46jNr1HHtM_eVTRKUpoiRA/"><img src="../img/social_youtube_2756.png" width="16"></a> 
+			</div>
+		</div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="/bootstrap_4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
